2d1c6cf9a4ffa8072be7a1572789ded7dbab1d99
VeraSapozhnikova <vera.sapozhnikova@mail.ru>
2023-03-04 20:00:59 +0300

    Requirements updated

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -11,6 +11,8 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
     "[::1]",
     "testserver",
+    "www.myfirstproject.pythonanywhere.com",
+    "myfirstproject.pythonanywhere.com",
 ]
 
 
@@ -26,6 +28,7 @@ INSTALLED_APPS = [
     "core.apps.CoreConfig",
     "about.apps.AboutConfig",
     "sorl.thumbnail",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -36,6 +39,11 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
 ]
 
 ROOT_URLCONF = "yatube.urls"
